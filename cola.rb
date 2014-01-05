2356a7b19e68274f97a5d03b7682a84fafb02a2e
claudiu <claudiu.garba@gmail.com>
2014-01-05 21:33:08 +0100

    dddd

--- cola.rb
+++ cola.rb
@@ -6,13 +6,20 @@ before do
   @throws = @defeat.keys
 end
 
-get '/' do
-  "Hello world!"
-end
-
 get '/throw/:type' do
-  player_throw = params[:type].to_sym
+  player_throw = params[:type].to_s
+  compute_throw = @throws.sample
   if @throws.include?(player_throw)
     halt 403, "you must add: #{@throws}"
   end
+  if player_throw == compute_throw
+    "you tied with computer"
+  elsif compute_throw == @defeat[player_throw]
+    "nice! #{player_throw} beats #{compute_throw}"
+  else
+    "You just loose #{compute_throw} beats #{player_throw}"
+  end
+  
+      
+
 end
