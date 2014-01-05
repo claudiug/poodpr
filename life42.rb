f5b80eb4c9ede74a1786ebeed93806c3c21ec4e0
claudiu <claudiu.garba@gmail.com>
2014-01-05 23:52:53 +0100

    add some stack for me

--- life42.rb
+++ life42.rb
@@ -1,3 +1,27 @@
-class Life42
-  puts self.class
+module StackLike
+  def stack
+    @stack ||= []
+  end
+
+  def add_to_stack(object)
+    stack.push(object)
+  end
+
+  def take_from_stack
+    stack.pop
+  end
+
+  def to_s
+    @stack.each{|s| puts s}
+  end
 end
+
+class Demo
+  include StackLike
+end
+
+demo = Demo.new
+demo.add_to_stack(1)
+demo.add_to_stack(2)
+demo.add_to_stack(3)
+puts demo
